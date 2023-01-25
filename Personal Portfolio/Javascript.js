74c0adeb99e05f7d02e5a001c0ad264d54138e88
pyun1023 <pyun1023@gmail.com>
2023-01-25 03:51:49 -0500

    assignment

---
+++ Personal Portfolio/Javascript.js
@@ -0,0 +1,10 @@
+Javascript
+var arrayofnumbers=[1,3,5];
+var sum= arrayofnumbers[0];
+
+for (var i=0; i<arrayofnumbers.length;i++) {sum+= arrayofnumbers[i];
+console.log ("the current sum is: " +sum);
+}
+console.log("the total sum is: " + sum);
+
+
